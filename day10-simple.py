772026f9a94ea4c68c0ee5b40e4a9dc3afbd6ed1
Cromski <90246462+Cromski@users.noreply.github.com>
2024-12-10 11:17:35 +0100

    simplified solution for day 10

---
+++ day10-simple.py
@@ -0,0 +1,34 @@
+with open("day10-data.txt", 'r') as file:
+    rows = [list(map(int,row.strip())) for row in file.readlines()]
+
+countPt1 = 0
+countPt2 = 0
+
+############# PART 1 & 2 #############
+def theAlg(y,x,prevNum, knownLocations):
+    global countPt1, countPt2
+    try:
+        if x < 0 or y < 0 or x >= len(rows[y]) or y >= len(rows): return 0
+    except:
+        return 
+    
+    if prevNum != rows[y][x]-1: return 
+    
+    if rows[y][x] == 9:
+        countPt2+=1
+        if ((y,x) not in knownLocations):
+            knownLocations.append((y,x))
+            countPt1 +=1
+    
+    theAlg(y-1,x,prevNum+1, knownLocations)
+    theAlg(y,x+1,prevNum+1, knownLocations)
+    theAlg(y+1,x,prevNum+1, knownLocations)
+    theAlg(y,x-1,prevNum+1, knownLocations)
+
+for i,r in enumerate(rows):
+    for j, num in enumerate(r):
+        if(num == 0):
+            theAlg(i,j,-1,[])
+
+print(f"pt1: {countPt1}")
+print(f"pt2: {countPt2}")
