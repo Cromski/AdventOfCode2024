10ddb5bec166ee61e0fe83fad5d892d3a89a6e49
Cromski <90246462+Cromski@users.noreply.github.com>
2024-12-11 17:06:30 +0100

    Update day11.py pep8 i think

--- day11.py
+++ day11.py
@@ -5,23 +5,23 @@ with open("day11-data.txt", 'r') as file:
     stones = list(map(int,file.readline().strip().split()))
 
 @functools.cache
-def blinkSingleStone(blinks,stone):
+def blink_single_stone(blinks, stone):
     if blinks == 0:
         return 1
     else:
         res = 0
-        if stone == 0: res += blinkSingleStone(blinks-1,1)
+        if stone == 0: res += blink_single_stone(blinks-1,1)
         elif len(str(stone)) % 2 == 0:
-            res += blinkSingleStone(blinks-1,int(str(stone)[:len(str(stone))//2]))
-            res += blinkSingleStone(blinks-1,int(str(stone)[len(str(stone))//2:]))
-        else: res += blinkSingleStone(blinks-1,stone*2024)
+            res += blink_single_stone(blinks-1,int(str(stone)[:len(str(stone))//2]))
+            res += blink_single_stone(blinks-1,int(str(stone)[len(str(stone))//2:]))
+        else: res += blink_single_stone(blinks-1,stone*2024)
         return res
 
-def doIt(blinks):
-    totalStones = 0
+def do_the_blinks(blinks):
+    total_stones = 0
     for stone in stones:
-        totalStones += blinkSingleStone(blinks,stone)
-    return totalStones
+        total_stones += blink_single_stone(blinks, stone)
+    return total_stones
 
-print(f"pt1: {doIt(25)}, Time: {time.time() - start_time}")
-print(f"pt2: {doIt(75)}, Time: {time.time() - start_time}")
+print(f"pt1: {do_the_blinks(25)}, Time: {time.time() - start_time}")
+print(f"pt2: {do_the_blinks(75)}, Time: {time.time() - start_time}")
